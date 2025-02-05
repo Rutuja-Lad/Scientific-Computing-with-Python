ef1ec57c82a003ade8d27398460fb4238f887a55
Rutuja Lad <121627262+Rutuja-Lad@users.noreply.github.com>
2025-02-05 15:00:59 +0530

    Add files via upload

---
+++ simpleCalculator.py
@@ -0,0 +1,10 @@
+a=100
+b=5
+print("**Calculations**")
+print("Addition of ",a,"and",b,"is",a+b)
+print("Substraction of",a,"and",b,"is",a-b)
+print("Multiplication of",a,"and",b,"is",a*b)
+print("Divisin of",a,"and",b,"is",a/b)
+print("Modulus of",a,"and",b,"is",a%b)
+print("Floar division of",a,"and",b,"is",a//b)
+print("Exponential of",a,"and",b,"is",a**3)
